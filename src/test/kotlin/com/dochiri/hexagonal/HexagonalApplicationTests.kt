9a71701428e7f1c8fa4adaa4f966caab85fe11bc
송성빈 <dochiri0916@gmail.com>
2026-02-20 00:05:02 +0900

    test: HexagonalApplicationTests.kt 추가

---
+++ src/test/kotlin/com/dochiri/hexagonal/HexagonalApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.dochiri.hexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
