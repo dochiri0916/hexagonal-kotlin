08be538def2b0368ec1c2a527f34f18c60cf7bfe
송성빈 <dochiri0916@gmail.com>
2026-02-20 00:05:01 +0900

    refactor: CorsProperties.kt 추가

---
+++ src/main/kotlin/com/dochiri/hexagonal/infrastructure/security/CorsProperties.kt
@@ -0,0 +1,8 @@
+package com.dochiri.hexagonal.infrastructure.security
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "cors")
+data class CorsProperties(
+    val allowedOrigins: List<String>
+)
