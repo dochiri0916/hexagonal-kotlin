889e7894fdd8b44ca9ed70640ea0909d7c8da716
송성빈 <dochiri0916@gmail.com>
2026-02-20 00:04:57 +0900

    refactor: PasswordHashPort.java 제거

--- src/main/java/com/dochiri/hexagonal/application/user/port/out/PasswordHashPort.java
+++
@@ -1,9 +0,0 @@
-package com.dochiri.hexagonal.application.user.port.out;
-
-public interface PasswordHashPort {
-
-    String encode(String rawPassword);
-
-    boolean matches(String rawPassword, String encodedPassword);
-
-}
