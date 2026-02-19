08ec229d7944df7818d30616aaa8e73820ad05d8
송성빈 <dochiri0916@gmail.com>
2026-02-20 00:04:58 +0900

    refactor: InvalidPasswordException.java 제거

--- src/main/java/com/dochiri/hexagonal/domain/user/exception/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.dochiri.hexagonal.domain.user.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
