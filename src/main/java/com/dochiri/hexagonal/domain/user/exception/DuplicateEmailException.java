d233a8a23da7f78679ac3d6ee44ff07aa1faddf1
송성빈 <dochiri0916@gmail.com>
2026-02-20 00:04:58 +0900

    refactor: DuplicateEmailException.java 제거

--- src/main/java/com/dochiri/hexagonal/domain/user/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.dochiri.hexagonal.domain.user.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String email) {
-        super("이미 가입된 이메일입니다: " + email);
-    }
-}
