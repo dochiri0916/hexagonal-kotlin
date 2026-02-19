38e0a9d2e3b2573620c495fd4ab9d714f1f4efa2
송성빈 <dochiri0916@gmail.com>
2026-02-20 00:04:57 +0900

    refactor: GetUserProfileQuery.java 제거

--- src/main/java/com/dochiri/hexagonal/application/user/port/in/GetUserProfileQuery.java
+++
@@ -1,9 +0,0 @@
-package com.dochiri.hexagonal.application.user.port.in;
-
-import com.dochiri.hexagonal.application.user.dto.UserProfileResult;
-
-public interface GetUserProfileQuery {
-
-    UserProfileResult getProfile(String userPublicId);
-
-}
